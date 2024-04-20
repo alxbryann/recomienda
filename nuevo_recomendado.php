72e79d4397a727410edc42886a4227484d93afe8
alxbryann <bryanalexanderbogota@gmail.com>
2024-04-20 16:04:11 -0500

    testing nuevo_recomendado.php

--- nuevo_recomendado.php
+++ nuevo_recomendado.php
@@ -9,8 +9,11 @@ $host = "82.197.80.210";
 $connection = mysqli_connect($host, $user, $pass);
 
 //Llamamos al input del formulario
-$cod_rec = $_POST["codigo_recomendado"];
-$com_rec = $_POST["comentario_recomendado"];
+$id_usuario = 1;
+$id_recomendado = $_POST["id_recomendado"];
+$estrellas = 5;
+$especialidad = 1;
+$comentario = $_POST["comentario_recomendado"];
 
 //Nombre de la base de datos
 $datab = "u482925761_recomienda";
@@ -18,7 +21,7 @@ $datab = "u482925761_recomienda";
 $db = mysqli_select_db($connection,$datab);
 echo $nombre;
 //Diseñamos la instruccion sql
-$instruccion_sql = "insert into comentarios(nombre, email, mensaje) values ('$nombre', '$email', '$mensaje')";
+$instruccion_sql = "insert into recomendaciones(nombre, email, mensaje) values ('$nombre', '$email', '$mensaje')";
 $resultado = mysqli_query($connection, $instruccion_sql);
 ?>
 
