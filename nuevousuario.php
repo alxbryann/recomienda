9e4edee352b78289eeee876b8f6578960866f066
alxbryann <bryanalexanderbogota@gmail.com>
2024-04-24 21:02:34 -0500

    correcting nuevousuario.php

--- nuevousuario.php
+++ nuevousuario.php
@@ -18,17 +18,17 @@ if (is_post_request()) {
 
     $fields = [
         'email' => 'email | required | email | unique: usuarios, email_usuario',
-        'nombre'=>'string',
-        'apellido'=>'string',
-        'direccion'=>'string',
-        'telefono'=>'string',
-        'pais'=>'int',
-        'departamento'=>'int',
-        'municipio'=>'int',    
+        'nombre' => 'string',
+        'apellido' => 'string',
+        'direccion' => 'string',
+        'telefono' => 'string',
+        'pais' => 'int',
+        'departamento' => 'int',
+        'municipio' => 'int',
         'password' => 'string | required | secure',
         'password2' => 'string | required | same: password',
         'agree' => 'string | required',
-        'activation_code'=>'string',
+        'activation_code' => 'string',
     ];
 
     // custom messages
@@ -50,13 +50,25 @@ if (is_post_request()) {
             'errors' => $errors
         ]);
     }
-    $activation_code=generate_activation_code();
+    $activation_code = generate_activation_code();
 
-    if (register_user($inputs['email'], $inputs['nombre'], $inputs['apellido'],$inputs['direccion'], $inputs['telefono'],$inputs['pais'],$inputs['departamento'], 
-    $inputs['municipio'], $inputs['password'], $activation_code)) {
+    if (
+        register_user(
+            $inputs['email'],
+            $inputs['nombre'],
+            $inputs['apellido'],
+            $inputs['direccion'],
+            $inputs['telefono'],
+            $inputs['pais'],
+            $inputs['departamento'],
+            $inputs['municipio'],
+            $inputs['password'],
+            $activation_code
+        )
+    ) {
 
-        send_activation_email($inputs['email'],$activation_code);
-        
+        send_activation_email($inputs['email'], $activation_code);
+
         redirect_with_message(
             'login.php',
             'Your account has been created successfully. Please login here.'
@@ -64,131 +76,128 @@ if (is_post_request()) {
 
     }
 
-    } else if (is_get_request()) {
-        [$inputs, $errors] = session_flash('inputs', 'errors'); 
-    }
+} else if (is_get_request()) {
+    [$inputs, $errors] = session_flash('inputs', 'errors');
+}
 
-?>  
+?>
 <?php view('header', ['title' => 'Nuevo Usuario']) ?>
-            <link rel="stylesheet" href="estilosGenerales.css">
-            <h2>Registro nuevo usuario</h2>
-            <div>
-                <form method="post" action="nuevousuario.php">
-                <div>
-                    <label for="email">E-mail:</label>
-                    <input type="email" name="email" id="email" placeholder="e-mail"value="<?= $inputs['email'] ?? '' ?>"
-                        class="<?= error_class($errors, 'email') ?>">
-                        <small><?= $errors['email'] ?? '' ?></small>
-                    </div>    
+<link rel="stylesheet" href="estilosGenerales.css">
+<h2>Registro nuevo usuario</h2>
 
-                    <label for="NombreUsuario">Nombre:</label>
-                    <input type="text" name="nombre" id="nombre" placeholder="Primer nombre">
-                    <label for="ApellidoUsuario">Apellido:</label>
-                    <input type="text" name="apellido" id="apellido" placeholder="Apellido usuario">
-                    
-                    <div>
-                        <label for="password">Contrasena:</label>
-                        <input type="password" name="password" id="password" placeholder="contrasena usuario" value="<?= $inputs['password'] ?? '' ?>"
-                            class="<?= error_class($errors, 'password') ?>">
-                        <small><?= $errors['password'] ?? '' ?></small>
-                    </div>
-                    
-                    <div>
-                        <label for="password2">Repetir Contrasena:</label>
-                        <input type="password" name="password2" id="password2" placeholder="Repetir Contrasena usuario" value="<?= $inputs['password2'] ?? '' ?>"
-                            class="<?= error_class($errors, 'password2') ?>">
-                        <small><?= $errors['password2'] ?? '' ?></small>
-                    </div>
+<form method="post" action="nuevousuario.php">
 
-                      
-                    <label for="DirUsuario">Direccion:</label>
-                    <input type="text" name="direccion" id="direccion" placeholder="Direccion -">
-                    <label for="TelefonoUsuario" >Telefono:</label>
-                    <input type="text" name="telefono" id="telefono" placeholder="numero de telefono" >
-                    <label for="pais">Pais:</label>
-                    <select id="pais" name="pais">
-                        <option value="">Selecciones pais</option>
-                        <?php
-                            //require_once 'db_conn.php';
-                            try
-                            { 
-                                $pdo=new PDO($attr,$user,$pass,$opts);
-                            }
-                            catch (PDOException $e)
-                            {
-                                throw new PDOException($e->getMessage(),(int)$e->getCode());
-                            }
-                            $query ="SELECT * FROM paises";
-                            $result=$pdo->query($query);
-                            while ($row=$result->fetch(PDO::FETCH_BOTH))
-                            {
-                            ?>
-                            <option value="<?php echo $row['cod_pais']; ?>"><?php echo $row['nombre_pais']; ?></option>
-                            <?php
-                            }
-                            ?>
-                        </select>
-                    <label for="Departamento" >Departamento:</label>
-                    <select id="id_depto" name="departamento"> 
-                        <option disabled="" selected="">Seleccione Departamaneto</option>
-                    </select>
-                    <label for="Municipio" >Municipio:</label>
-                    <select id="id_mun" name="municipio"> 
-                        <option disabled="" selected="">Seleccione municipio</option>
-                    </select>
-
-                    <div>
-                        <label for="agree" >
-                            <input type="checkbox" name="agree" id="agree"  value="checked" <?= $inputs['agree'] ?? '' ?> /> 
-                            Estoy de acuerdo con los
-                            <a href="#" title="term of services" >terminos de servicio</a>
-                        </label>
-                        <small><?= $errors['agree'] ?? '' ?></small>
-                    </div>
-                    <input type="submit" value="Guardar" >
-                    <footer>Already a member? <a href="login.php">Login here</a></footer> 
-                </form>
-            </div>
-            <script>
-	            $(document).ready(function() {
-                $("#pais").on('change', function() {
-                var countryid = $(this).val();
-
-                $.ajax({
-                    method: "POST",
-                    url: "consulta.php",
-                    data: {
-                        id: countryid
-                    },
-                    datatype: "html",
-                    success: function(data) {
-                        $("#id_depto").html(data);
-                        $("#id_mun").html('<option value="">Seleccione municipio</option');
-
-                        }
-                    });
-                });
+    <label for="email">E-mail:</label>
+    <input type="email" name="email" id="email" placeholder="e-mail" value="<?= $inputs['email'] ?? '' ?>"
+        class="<?= error_class($errors, 'email') ?>">
+    <small><?= $errors['email'] ?? '' ?></small>
 
 
-		        //City Selection
-		        $("#id_depto").on('change', function() {
-                    var stateid = $(this).val();
-                $.ajax({
-                    method: "POST",
-                    url: "consulta.php",
-                    data: {
+    <label for="NombreUsuario">Nombre:</label>
+    <input type="text" name="nombre" id="nombre" placeholder="Primer nombre">
+    <label for="ApellidoUsuario">Apellido:</label>
+    <input type="text" name="apellido" id="apellido" placeholder="Apellido usuario">
+
+
+    <label for="password">Contrasena:</label>
+    <input type="password" name="password" id="password" placeholder="contrasena usuario"
+        value="<?= $inputs['password'] ?? '' ?>" class="<?= error_class($errors, 'password') ?>">
+    <small><?= $errors['password'] ?? '' ?></small>
+
+
+
+    <label for="password2">Repetir Contrasena:</label>
+    <input type="password" name="password2" id="password2" placeholder="Repetir Contrasena usuario"
+        value="<?= $inputs['password2'] ?? '' ?>" class="<?= error_class($errors, 'password2') ?>">
+    <small><?= $errors['password2'] ?? '' ?></small>
+
+
+
+    <label for="DirUsuario">Direccion:</label>
+    <input type="text" name="direccion" id="direccion" placeholder="Direccion -">
+    <label for="TelefonoUsuario">Telefono:</label>
+    <input type="text" name="telefono" id="telefono" placeholder="numero de telefono">
+    <label for="pais">Pais:</label>
+    <select id="pais" name="pais">
+        <option value="">Selecciones pais</option>
+        <?php
+        //require_once 'db_conn.php';
+        try {
+            $pdo = new PDO($attr, $user, $pass, $opts);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int) $e->getCode());
+        }
+        $query = "SELECT * FROM paises";
+        $result = $pdo->query($query);
+        while ($row = $result->fetch(PDO::FETCH_BOTH)) {
+            ?>
+            <option value="<?php echo $row['cod_pais']; ?>"><?php echo $row['nombre_pais']; ?></option>
+            <?php
+        }
+        ?>
+    </select>
+    <label for="Departamento">Departamento:</label>
+    <select id="id_depto" name="departamento">
+        <option disabled="" selected="">Seleccione Departamaneto</option>
+    </select>
+    <label for="Municipio">Municipio:</label>
+    <select id="id_mun" name="municipio">
+        <option disabled="" selected="">Seleccione municipio</option>
+    </select>
+
+
+    <label for="agree">
+        <input type="checkbox" name="agree" id="agree" value="checked" <?= $inputs['agree'] ?? '' ?> />
+        Estoy de acuerdo con los
+        <a href="#" title="term of services">terminos de servicio</a>
+    </label>
+    <small><?= $errors['agree'] ?? '' ?></small>
+
+    <input type="submit" value="Guardar">
+    <footer>Already a member? <a href="login.php">Login here</a></footer>
+</form>
+
+<script>
+    $(document).ready(function () {
+        $("#pais").on('change', function () {
+            var countryid = $(this).val();
+
+            $.ajax({
+                method: "POST",
+                url: "consulta.php",
+                data: {
+                    id: countryid
+                },
+                datatype: "html",
+                success: function (data) {
+                    $("#id_depto").html(data);
+                    $("#id_mun").html('<option value="">Seleccione municipio</option');
+
+                }
+            });
+        });
+
+
+        //City Selection
+        $("#id_depto").on('change', function () {
+            var stateid = $(this).val();
+            $.ajax({
+                method: "POST",
+                url: "consulta.php",
+                data: {
                     sid: stateid
-                    },
-                    datatype: "html",
-                    success: function(data) {
+                },
+                datatype: "html",
+                success: function (data) {
                     $("#id_mun").html(data);
 
                 }
 
-                });
-             });
             });
-            </script>  
-            
-    </body>
-    </html>
+        });
+    });
+</script>
+
+</body>
+
+</html>
