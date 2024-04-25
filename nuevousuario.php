c49e1823e5f8527afbc20dfff8542ad6f7698055
alxbryann <bryanalexanderbogota@gmail.com>
2024-04-24 21:06:54 -0500

    testing styles

--- nuevousuario.php
+++ nuevousuario.php
@@ -83,80 +83,80 @@ if (is_post_request()) {
 ?>
 <?php view('header', ['title' => 'Nuevo Usuario']) ?>
 <link rel="stylesheet" href="estilosGenerales.css">
-<h2>Registro nuevo usuario</h2>
 
-<form method="post" action="nuevousuario.php">
-
-    <label for="email">E-mail:</label>
-    <input type="email" name="email" id="email" placeholder="e-mail" value="<?= $inputs['email'] ?? '' ?>"
-        class="<?= error_class($errors, 'email') ?>">
-    <small><?= $errors['email'] ?? '' ?></small>
-
-
-    <label for="NombreUsuario">Nombre:</label>
-    <input type="text" name="nombre" id="nombre" placeholder="Primer nombre">
-    <label for="ApellidoUsuario">Apellido:</label>
-    <input type="text" name="apellido" id="apellido" placeholder="Apellido usuario">
+<div class="register-container">
+    <form method="post" action="nuevousuario.php">
+        <h2>Modulo de registro</h2>
+        <label for="email">E-mail:</label>
+        <input type="email" name="email" id="email" placeholder="e-mail" value="<?= $inputs['email'] ?? '' ?>"
+            class="<?= error_class($errors, 'email') ?>">
+        <small><?= $errors['email'] ?? '' ?></small>
 
 
-    <label for="password">Contrasena:</label>
-    <input type="password" name="password" id="password" placeholder="contrasena usuario"
-        value="<?= $inputs['password'] ?? '' ?>" class="<?= error_class($errors, 'password') ?>">
-    <small><?= $errors['password'] ?? '' ?></small>
+        <label for="NombreUsuario">Nombre:</label>
+        <input type="text" name="nombre" id="nombre" placeholder="Primer nombre">
+        <label for="ApellidoUsuario">Apellido:</label>
+        <input type="text" name="apellido" id="apellido" placeholder="Apellido usuario">
 
 
-
-    <label for="password2">Repetir Contrasena:</label>
-    <input type="password" name="password2" id="password2" placeholder="Repetir Contrasena usuario"
-        value="<?= $inputs['password2'] ?? '' ?>" class="<?= error_class($errors, 'password2') ?>">
-    <small><?= $errors['password2'] ?? '' ?></small>
+        <label for="password">Contrasena:</label>
+        <input type="password" name="password" id="password" placeholder="contrasena usuario"
+            value="<?= $inputs['password'] ?? '' ?>" class="<?= error_class($errors, 'password') ?>">
+        <small><?= $errors['password'] ?? '' ?></small>
 
 
 
-    <label for="DirUsuario">Direccion:</label>
-    <input type="text" name="direccion" id="direccion" placeholder="Direccion -">
-    <label for="TelefonoUsuario">Telefono:</label>
-    <input type="text" name="telefono" id="telefono" placeholder="numero de telefono">
-    <label for="pais">Pais:</label>
-    <select id="pais" name="pais">
-        <option value="">Selecciones pais</option>
-        <?php
-        //require_once 'db_conn.php';
-        try {
-            $pdo = new PDO($attr, $user, $pass, $opts);
-        } catch (PDOException $e) {
-            throw new PDOException($e->getMessage(), (int) $e->getCode());
-        }
-        $query = "SELECT * FROM paises";
-        $result = $pdo->query($query);
-        while ($row = $result->fetch(PDO::FETCH_BOTH)) {
-            ?>
-            <option value="<?php echo $row['cod_pais']; ?>"><?php echo $row['nombre_pais']; ?></option>
+        <label for="password2">Repetir Contrasena:</label>
+        <input type="password" name="password2" id="password2" placeholder="Repetir Contrasena usuario"
+            value="<?= $inputs['password2'] ?? '' ?>" class="<?= error_class($errors, 'password2') ?>">
+        <small><?= $errors['password2'] ?? '' ?></small>
+
+
+
+        <label for="DirUsuario">Direccion:</label>
+        <input type="text" name="direccion" id="direccion" placeholder="Direccion -">
+        <label for="TelefonoUsuario">Telefono:</label>
+        <input type="text" name="telefono" id="telefono" placeholder="numero de telefono">
+        <label for="pais">Pais:</label>
+        <select id="pais" name="pais">
+            <option value="">Selecciones pais</option>
             <?php
-        }
-        ?>
-    </select>
-    <label for="Departamento">Departamento:</label>
-    <select id="id_depto" name="departamento">
-        <option disabled="" selected="">Seleccione Departamaneto</option>
-    </select>
-    <label for="Municipio">Municipio:</label>
-    <select id="id_mun" name="municipio">
-        <option disabled="" selected="">Seleccione municipio</option>
-    </select>
+            //require_once 'db_conn.php';
+            try {
+                $pdo = new PDO($attr, $user, $pass, $opts);
+            } catch (PDOException $e) {
+                throw new PDOException($e->getMessage(), (int) $e->getCode());
+            }
+            $query = "SELECT * FROM paises";
+            $result = $pdo->query($query);
+            while ($row = $result->fetch(PDO::FETCH_BOTH)) {
+                ?>
+                <option value="<?php echo $row['cod_pais']; ?>"><?php echo $row['nombre_pais']; ?></option>
+                <?php
+            }
+            ?>
+        </select>
+        <label for="Departamento">Departamento:</label>
+        <select id="id_depto" name="departamento">
+            <option disabled="" selected="">Seleccione Departamaneto</option>
+        </select>
+        <label for="Municipio">Municipio:</label>
+        <select id="id_mun" name="municipio">
+            <option disabled="" selected="">Seleccione municipio</option>
+        </select>
 
 
-    <label for="agree">
-        <input type="checkbox" name="agree" id="agree" value="checked" <?= $inputs['agree'] ?? '' ?> />
-        Estoy de acuerdo con los
-        <a href="#" title="term of services">terminos de servicio</a>
-    </label>
-    <small><?= $errors['agree'] ?? '' ?></small>
+        <label for="agree">
+            <input type="checkbox" name="agree" id="agree" value="checked" <?= $inputs['agree'] ?? '' ?> />
+            Estoy de acuerdo con los
+            <a href="#" title="term of services">terminos de servicio</a>
+        </label>
+        <small><?= $errors['agree'] ?? '' ?></small>
 
-    <input type="submit" value="Guardar">
-    <footer>Already a member? <a href="login.php">Login here</a></footer>
-</form>
-
+        <input type="submit" value="Guardar">
+        <footer>Already a member? <a href="login.php">Login here</a></footer>
+    </form>
+</div>
 <script>
     $(document).ready(function () {
         $("#pais").on('change', function () {
