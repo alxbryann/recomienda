7f01a9c4a3a57d836b51db1bb736dc9466b2c16e
sgomezfajardo <128939273+sgomezfajardo@users.noreply.github.com>
2024-04-28 01:36:35 -0500

    prueba index.php

--- index.php
+++ index.php
@@ -63,9 +63,9 @@
     <div id="sesion" style="margin-left: 40px;">
         <?php
         if (isset($usuario)) {
-            echo '<div class="dropdown" style="position: relative; left: 90px;">';
+            echo '<div class="dropdown" style="position: relative; left: 2px;">';
             echo '<a href="#" class="dropdown-toggle" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: black;">Bienvenido, '. $usuario .'</a>';
-            echo '<ul class="dropdown-menu" aria-labelledby="dropdownMenuLink" style="margin-left: 40px;">';
+            echo '<ul class="dropdown-menu" aria-labelledby="dropdownMenuLink" style="margin-left: 80px;">';
             echo '<li><a class="dropdown-item" href="#" style="font-size: 14px; color: black;">Ver perfil</a></li>';
             echo '<li><a class="dropdown-item" href="#" style="font-size: 14px; color: black;">Cerrar sesión</a></li>';
             echo '</ul>';
