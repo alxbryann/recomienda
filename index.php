1d0e3349d4d85b38651cdda75e75ac99cbad0116
alxbryann <bryanalexanderbogota@gmail.com>
2024-04-28 19:49:06 -0500

    testing index.php

--- index.php
+++ index.php
@@ -59,16 +59,16 @@
         </a>
     </div>
     <div id="tituloTexto">
-        <h1>¡Bienvenido a recomienda!</h1>
+        <h1 style="margin-top: 40px; margin-left: 60px; color: white;">¡Bienvenido a recomienda!</h1>
     </div>
-    <div id="sesion">
+    <div id="sesion" style="margin-left: 40px;">
         <?php
         if (isset($nombre)) {
-            echo '<div class="dropdown" style="position: relative; top: 30px ;left: -1px;">';
+            echo '<div class="dropdown" style="position: relative; top: 40px ;left: -1px;">';
             echo '<a href="#" class="dropdown-toggle" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">Bienvenido, '. $nombre .'</a>';
             echo '<ul class="dropdown-menu" aria-labelledby="dropdownMenuLink" style="margin-left: 40px;">';
-            echo '<li><a class="dropdown-item" href="#" style="font-size: 12px; color: black;">Ver perfil</a></li>';
-            echo '<li><a class="dropdown-item" href="cerrar_sesion.php" style="font-size: 12px; color: black;">Cerrar sesión</a></li>';
+            echo '<li><a class="dropdown-item" href="#" style="font-size: 12px; color: black;" z-index"3";>Ver perfil</a></li>';
+            echo '<li><a class="dropdown-item" href="cerrar_sesion.php" style="font-size: 12px; color: black;"  z-index"3">Cerrar sesión</a></li>';
             echo '</ul>';
             echo '</div>';
         } else {
